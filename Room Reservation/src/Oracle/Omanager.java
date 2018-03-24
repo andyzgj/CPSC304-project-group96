17fd2ae869fd48bbc55b1d31d99588b85d0abbe5
Andy <st8885423@hotmail.com>
2018-03-23 22:22:16 -0700

    little test 2

--- Room Reservation/src/Oracle/Omanager.java
+++ Room Reservation/src/Oracle/Omanager.java
@@ -8,65 +8,46 @@ import java.sql.Statement;
 public class Omanager {
 
 
-
-
-        private Connection conn;
+        private Connection c;
         private Statement stmt;
-
         private static Omanager instance = new Omanager();
 
         public Omanager() {
-
         }
 
         public static Omanager getInstance() {
-            return    instance;
+            return instance;
         }
 
         public Connection getConnection() {
-            if (conn == null) {
+            if (c == null) {
                 try {
                     DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
-                    // Class.forName("oracle.jdbc.driver.OracleDriver");
-                    //oracle.jdbc.driver.OracleDriver
-                    conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1522:ug", "ora_m1l0b", "a28488154");
-                    //stmt = conn.createStatement();
+                    c = DriverManager.getConnection("jdbc:oracle:thin:@dbhost.ugrad.cs.ubc.ca:1522:ug", "ora_v4l0b", "a26821158");
                     System.out.println("Connection succeeded");
                 } catch (SQLException e) {
                     e.printStackTrace();
-                    troubleshoot();
                 }
             }
 
-            return conn;
+            return c;
         }
 
-        private static void troubleshoot(){
-            System.out.println("Connection failed");
-            System.out.println("Troubleshooting 101:");
-            System.out.println("Have you connected to the VPN?");
-            System.out.println("Have you tunnelled into the department server?");
-            System.out.println("This can be done as follows:");
-            System.out.println("ssh -l csid -L localhost:1522:dbhost.ugrad.cs.ubc.ca:1522 thetis.ugrad.cs.ubc.ca");
-        }
-
-        //stringForUpdate is the sql command for executing Inserts, Updates, and Deletes in the DBA
-        public int execute(String stringForExecute){
+        public int execute(String executeString){
             int rowCount = -1;
             try {
-                //System.out.println(targetTable + " before execution -->");
-                rowCount = stmt.executeUpdate(stringForExecute);
+                rowCount = stmt.executeUpdate(executeString);
                 System.out.println("row " + rowCount + " updated");
             } catch (SQLException e) {
                 e.printStackTrace();
-                System.out.println(stringForExecute + " : update fails");
+                System.out.println(executeString + " : update fails");
             }
             return rowCount;
         }
 
         public void disconnect(){
             try {
-                conn.close();
+                c.close();
             } catch (SQLException e) {
                 e.printStackTrace();
                 System.out.println("disconnect fails");
---
+++ Room Reservation/src/Oracle/OraGuest.java
@@ -0,0 +1,105 @@
+package Oracle;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import Object.GuestInfo;
+
+public class OraGuest {
+
+
+        Omanager manager;
+        Connection c;
+
+        public OraGuest() {
+            manager = Omanager.getInstance();
+            c = manager.getConnection();
+        }
+
+        public List<GuestInfo> getGuest() {
+            List<GuestInfo> guests = new ArrayList<>();
+            try {
+                Statement st = c.createStatement();
+                ResultSet rs = st.executeQuery("select * from Guest");
+
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String name = rs.getString("name");
+                    Date birthday = rs.getDate("birthday");
+                    int phone = rs.getInt("phone_num");
+                    int credit = rs.getInt("credit_card_num");
+
+                    GuestInfo gi = new GuestInfo(id, name, birthday, phone, credit);
+                    guests.add(gi);
+                }
+
+            st.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            return guests;
+        }
+
+        public void InsertGuest(int id , String name , Date birthday, int phone, int credit ) {
+            PreparedStatement ps;
+
+            try {
+                ps = c.prepareStatement("INSERT INTO Guest VALUES (?,?,?,?,?)");
+                ps.setInt(1, id);
+
+                ps.setString(2, name);
+
+                ps.setDate(3, birthday);
+
+                ps.setInt(4, phone);
+
+                if (credit == 0) {
+                    ps.setNull(5, java.sql.Types.INTEGER);
+                } else {
+                    ps.setInt(5, credit);
+                }
+
+                ps.executeUpdate();
+                c.commit();
+                ps.close();
+            }
+            catch (SQLException ex)
+            {
+                System.out.println("Message: " + ex.getMessage());
+                try
+                {
+                    // undo the insert
+                    c.rollback();
+                }
+                catch (SQLException ex2)
+                {
+                    System.out.println("Message: " + ex2.getMessage());
+                    System.exit(-1);
+                }
+            }
+
+        }
+
+        public boolean updateGuestInfo( int id , String name , Date birthday, String phone, int credit_card_num) {
+            manager.getConnection();
+            int rowCount = manager.execute("UPDATE Guest SET name = "
+                    + name
+                    + " , birthday = "
+                    + birthday
+                    + ", phone = "
+                    + phone
+                    + ", credit_card_num = "
+                    + credit_card_num
+                    + " WHERE id = "
+                    + id);
+            manager.disconnect();
+            if (rowCount == 1)
+                return true;
+            else
+                return false;
+        }
+
+
+
+}
