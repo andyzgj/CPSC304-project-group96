5c875b3841aa417346fee5d8d9f4edd45935342c
Andy <st8885423@hotmail.com>
2018-03-23 21:29:27 -0700

    little test 2

--- Room Reservation/src/Oracle/Omanager.java
+++ Room Reservation/src/Oracle/Omanager.java
@@ -20,7 +20,7 @@ public class Omanager {
         }
 
         public static Omanager getInstance() {
-            return instance;
+            return    instance;
         }
 
         public Connection getConnection() {
