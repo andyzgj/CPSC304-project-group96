4aa9006968c5b522048c9a2b9f4fcc5b6c893004
lynch <lynch.liu@hotmail.com>
2018-03-25 12:25:30 -0700

    UI MODIFIED: BOOT

--- Room Reservation/src/UI/Boot.java
+++ Room Reservation/src/UI/Boot.java
@@ -1,9 +1,25 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Boot {
     private JButton guestButton;
     private JButton employeeButton;
-    private JPanel title;
+
+    public Boot() {
+        guestButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        employeeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
 }
--- Room Reservation/src/UI/GuestLogin.java
+++ Room Reservation/src/UI/GuestLogin.java
@@ -1,12 +1,49 @@
 package UI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GuestLogin {
+public class GuestLogin extends JFrame{
 
     public GuestLogin(){
 
+        registerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //go to register window
+                // TODO
+            }
+        });
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //log in with phone#
+                if(selectID = false){
+                    //call login
+                    //TODO
+                }
+                //log in with ID
+                else{
+                    //call login
+                    //TODO
+                }
+            }
+        });
+        phoneNumberRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selectID = false;
+            }
+        });
+        guestIDRadioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                selectID = true;
+            }
+        });
     }
+    private boolean selectID = false;
     private JPanel mainPanel;
     private JRadioButton phoneNumberRadioButton;
     private JButton registerButton;
@@ -21,3 +58,4 @@ public class GuestLogin {
     private JTextField selectLogInTypeTextField;
     private JPanel buttom;
 }
+
