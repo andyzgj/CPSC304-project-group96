6f92e948d0e33fe9b1141f23580cc8ea8c26139e
Andy <st8885423@hotmail.com>
2018-03-24 20:25:49 -0700

    little test 2

--- Room Reservation/src/Object/VIPInfo.java
+++ Room Reservation/src/Object/VIPInfo.java
@@ -2,15 +2,21 @@ package Object;
 
 import java.sql.Date;
 
-public class VIPInfo extends GuestInfo{
+public class VIPInfo{
 
+    int id;
     int points;
 
-    public VIPInfo(int id, String name, Date birthday, int phone_num, int credit_card_num, int points){
-        super(id,name,birthday,phone_num,credit_card_num);
-        this.points =  points;
-    }
+   // public VIPInfo(int id, String name, Date birthday, int phone_num, int credit_card_num, int points){
+      //  super(id,name,birthday,phone_num,credit_card_num);
+    //    this.points =  points;
+  //  }
+   public VIPInfo(int id, int points) {
+       this.id = id;
+       this.points = points;
+   }
 
+     public int getID(){return id;}
 
      public int getPoints(){return points;}
 
